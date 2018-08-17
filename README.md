be7a8c22298de73110b2c62b4046644ed3a37c7d
Jonathon Hill <jhill9693@gmail.com>
2018-08-17 11:25:03 -0400

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# passport-compose
+
+Utilities for chaining multiple Passport authentication middlewares
+
+## Use case
+
+You have multiple Passport authentication strategies, and you need _all_ of them to run before considering the user to be authenticated.
+
+For instance, you may wish to authenticate with username and password (via passport-local), then also pass an OTP or CAPTCHA check in a separate strategy.
+
+## Requirements
+
+* Node.js 8+
+* Passport strategies of your choice
+
+## Installation
+
+```bash
+$ npm install --save passport-compose
+```
+
+## Usage
+
+```javascript
+const passportCompose = require("passport-compose");
+const { compose, loginRedirect, isAuthenticated } = passportCompose({
+  // optional configuration
+  sessionStageField: "passport.stage",
+  sessionLoginRedirectField: "redirectTo",
+  successRedirect: "/",
+  successReturnToOrRedirect: "/",
+  failureRedirect: "/login"
+});
+
+// This example assumes you have set up an express application called "app",
+// and have required and configured "local" and "otp" strategies for passport
+
+// Do not set "successRedirect" or "successReturnToOrRedirect" here, or a
+// passing strategy will redirect before the next one runs
+const localSettings = { failureRedirect: "/login" };
+const otpSettings = { failureRedirect: "/login" };
+
+// Require successful username+password and OTP authentication, then redirect
+const strategies = [
+  passport.authenticate("local", localSettings)),
+  passport.authenticate("otp", otpSettings))
+];
+app.post("/login", compose(strategies), loginRedirect());
+
+// To require all strategies to pass before accessing a resource:
+app.get("/protected", isAuthenticated(), (req, res) => {
+  res.sendStatus(204);
+});
+```
+
+## License
+
+MIT license
---
+++ index.js
@@ -0,0 +1,95 @@
+const { get, set, defaultsDeep } = require("lodash");
+const debug = require("debug")("passport-compose");
+
+const defaults = {
+  sessionStageField: "passport.stage",
+  sessionLoginRedirectField: "redirectTo",
+  successRedirect: "/",
+  successReturnToOrRedirect: "/",
+  failureRedirect: "/login"
+};
+
+module.exports = (options = {}) => {
+  const config = defaultsDeep({}, options, defaults);
+
+  function setAuthenticationStage(req, stage) {
+    set(req.session, config.sessionStageField, stage);
+  }
+
+  function getAuthenticationStage(req) {
+    return get(req.session, config.sessionStageField, 0);
+  }
+
+  function isAuthenticatedStage(req, stage) {
+    return req.isAuthenticated() && getAuthenticationStage(req) >= stage;
+  }
+
+  function strategyMiddleware(strategy, stage) {
+    return (req, res, next) => {
+      const currentStage = getAuthenticationStage(req) || 0;
+
+      if (currentStage > stage) {
+        debug("skipping stage", stage);
+        next();
+      } else {
+        debug("authentication stage", stage);
+        setAuthenticationStage(req, stage);
+        if (strategy) {
+          strategy(req, res, next);
+        } else {
+          next(); // final strategy ran
+        }
+      }
+    };
+  }
+
+  function compose(...strategies) {
+    if (strategies.length === 0) {
+      throw new Error(
+        "At least one Passport authentication middleware is required"
+      );
+    }
+    config.strategies = strategies;
+    return [...strategies, undefined].map(strategyMiddleware);
+  }
+
+  function isAuthenticated(req, res, next) {
+    const stage = config.strategies.length;
+
+    if (isAuthenticatedStage(req, stage)) {
+      debug("authenticated, stage", stage);
+      return next();
+    } else {
+      debug("not authenticated, stage", stage);
+      if (!has(req.session, config.sessionLoginRedirectField)) {
+        set(req.session, config.sessionLoginRedirectField, req.originalUrl);
+      }
+      res.redirect(config.failureRedirect);
+    }
+  }
+
+  function loginRedirect(req, res, next) {
+    const stage = config.strategies.length;
+
+    if (isAuthenticatedStage(req, stage)) {
+      const url = config.successReturnToOrRedirect
+        ? get(
+            req.session,
+            config.sessionLoginRedirectField,
+            config.successReturnToOrRedirect
+          )
+        : config.successRedirect;
+      delete req.session[config.sessionLoginRedirectField];
+      res.redirect(url);
+    } else {
+      next();
+    }
+  }
+
+  return {
+    compose,
+    getAuthenticationStage,
+    isAuthenticated: () => isAuthenticated,
+    loginRedirect: () => loginRedirect
+  };
+};
